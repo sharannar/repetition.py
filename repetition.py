44a5a1096c8c9b670d6b7657eca17dde0a33e4e0
Sharanna Rushing <83149036+sharannar@users.noreply.github.com>
2023-11-14 21:05:08 -0500

    Update repetition.py

--- repetition.py
+++ repetition.py
@@ -1,12 +1,12 @@
 def say_hello(name):
     print('Hello, ', name, '!', sep='')
 
-def insert_separator(s="="):
-    print(s, s, s, sep="")
+def insert_separator(s="=", repeat = 30):
+    print(s* repeat)
 
 def recite_poem():
     print("How about a Monty Python poem?")
-    insert_separator("-")
+    insert_separator("-",20)
     print("Much to his Mum and Dad’s dismay")
     print("Horace ate himself one day.")
     print("He didn’t stop to say his grace,")
@@ -17,7 +17,7 @@ def say_goodbye(name):
 
 def main():
     your_name = input('What is your name? ')
-    insert_separator("-")
+    insert_separator("-", 20)
     say_hello(your_name)
     insert_separator()
     recite_poem()
